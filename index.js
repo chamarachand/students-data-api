17395fec2d7bafd7fb5817bb92fa4996a9b7a554
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-18 19:08:49 +0530

    Create middleware to assign requests with /api path to studentRoutes.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const studentRoutes = require("./routes/studentRoutes");
 
 app.use(express.json());
+app.use("/api", studentRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
