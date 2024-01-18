6d41eebe74b74a5fe6ddb76dc6b418de687b5995
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-18 19:29:25 +0530

    Create the first get route handler for /api

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(express.json());
 app.use("/api", studentRoutes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+app.listen(port, () => console.log(`Listening to port ${port}..`));
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 //Get
+router.get("/", (req, res) => {
+  res.send("Welcome to student database");
+});
+
+module.exports = router;
