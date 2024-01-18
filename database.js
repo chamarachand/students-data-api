67e543f8406e45166f5fae0d04f85b40e2020ac7
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-18 21:24:25 +0530

    Create models directory

--- database.js
+++ database.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const connection = () => {
   const url = "mongodb://localhost:27017/students-db";
   mongoose
-    .connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(url)
     .then(() => console.log("Connected to MongoDB.."))
     .catch((error) => console.error("Could not connect to MongoDB", error));
 };
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const studentRoutes = require("./routes/studentRoutes");
+const connection = require("./database");
 
+// Connecting to MongoDB
+connection();
+
+// Middleware
 app.use(express.json());
 app.use("/api", studentRoutes);
 
+// Connecting to the port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}..`));
