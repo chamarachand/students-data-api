9630fe62af556c262e08f8c9ead9742c62885e33
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-19 16:10:47 +0530

    Add validations to the schema

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,28 +3,38 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-  studentId: Number,
-  firstName: String,
-  lastName: String,
-  gender: String,
-  dateOfBirth: Date,
+  studentId: { type: Number, unique: true, required: true },
+  firstName: { type: String, minlength: 1, maxlength: 50 },
+  lastName: { type: String, minlength: 1, maxlength: 50 },
+  gender: { type: String, enum: ["male", "female"] },
+  dateOfBirth: { type: Date, required: true },
   contact: {
-    phone: String,
-    email: String,
-    address: String,
+    phone: { type: String, minlength: 10, maxlength: 15, required: true },
+    email: {
+      type: String,
+      unique: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    address: { type: String, minlength: 5, maxlength: 255 },
   },
   guardianInfo: {
-    name: String,
+    name: { type: String, minlength: 1, maxlength: 75 },
     contact: {
-      phone: String,
-      email: String,
+      phone: { type: String, minlength: 10, maxlength: 15, required: true },
+      email: {
+        type: String,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+      },
     },
   },
-  isActive: Boolean,
-  semFeesPaid: Boolean,
-  hasLibraryAccess: Boolean,
-  doingSports: Boolean,
-  inClubs: Boolean,
+  isActive: { type: Boolean, default: true },
+  semFeesPaid: { type: Boolean, default: false },
+  hasLibraryAccess: { type: Boolean, default: false },
+  doingSports: { type: Boolean, default: false },
+  inClubs: { type: Boolean, default: false },
 });
 
 //Get
