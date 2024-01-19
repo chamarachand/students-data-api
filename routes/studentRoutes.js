964fc29fbad94a463df3f25ca981ec602680d774
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-19 15:48:18 +0530

    Create a basic mongoose schema for students

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  studentId: Number,
+  firstName: String,
+  lastName: String,
+  gender: String,
+  dateOfBirth: Date,
+  contact: {
+    phone: String,
+    email: String,
+    address: String,
+  },
+  guardianInfo: {
+    name: String,
+    contact: {
+      phone: String,
+      email: String,
+    },
+  },
+  isActive: Boolean,
+  semFeesPaid: Boolean,
+  hasLibraryAccess: Boolean,
+  doingSports: Boolean,
+  inClubs: Boolean,
+});
 
 //Get
 router.get("/", (req, res) => {
   res.send("Welcome to student database");
 });
+
+router.get("/students", (req, res) => {});
 
 module.exports = router;
