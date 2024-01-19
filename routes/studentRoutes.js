29e5e097df6d8b9adff9749a4fe8285d3eba73c4
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-19 16:19:16 +0530

    Create seperate schema for contact information. Add further validation for dateOfBirth field in the studentSchema

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,34 +2,34 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+const contactSchema = new mongoose.Schema({
+  phone: { type: String, minlength: 10, maxlength: 15, required: true },
+  email: {
+    type: String,
+    unique: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  address: { type: String, minlength: 5, maxlength: 255 },
+});
+
 const studentSchema = new mongoose.Schema({
   studentId: { type: Number, unique: true, required: true },
   firstName: { type: String, minlength: 1, maxlength: 50 },
   lastName: { type: String, minlength: 1, maxlength: 50 },
   gender: { type: String, enum: ["male", "female"] },
-  dateOfBirth: { type: Date, required: true },
-  contact: {
-    phone: { type: String, minlength: 10, maxlength: 15, required: true },
-    email: {
-      type: String,
-      unique: true,
-      minlength: 5,
-      maxlength: 255,
-    },
-    address: { type: String, minlength: 5, maxlength: 255 },
-  },
-  guardianInfo: {
-    name: { type: String, minlength: 1, maxlength: 75 },
-    contact: {
-      phone: { type: String, minlength: 10, maxlength: 15, required: true },
-      email: {
-        type: String,
-        unique: true,
-        minlength: 5,
-        maxlength: 255,
+  dateOfBirth: {
+    type: Date,
+    validate: {
+      validator: function (date) {
+        date < Date.now();
       },
+      message: "Birthdate cannot be today or in future!",
     },
+    required: true,
   },
+  contact: contactSchema,
+  guardianInfo: contactSchema,
   isActive: { type: Boolean, default: true },
   semFeesPaid: { type: Boolean, default: false },
   hasLibraryAccess: { type: Boolean, default: false },
