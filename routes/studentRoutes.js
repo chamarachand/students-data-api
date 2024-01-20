97f142fc218c707902ed7f6ee53517a1462ede8c
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-20 15:15:30 +0530

    Create seperate student model (student.js) to handle the both Joi and mongoose validation logic

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,48 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Joi = require("joi");
-
-const contactSchema = new mongoose.Schema({
-  phone: { type: String, minlength: 10, maxlength: 15, required: true },
-  email: {
-    type: String,
-    minlength: 5,
-    maxlength: 255,
-    unique: true,
-    required: true,
-  },
-});
-
-const studentSchema = new mongoose.Schema({
-  studentId: { type: Number, unique: true, required: true },
-  firstName: { type: String, minlength: 1, maxlength: 50 },
-  lastName: { type: String, minlength: 1, maxlength: 50 },
-  gender: { type: String, enum: ["male", "female"] },
-  dateOfBirth: {
-    type: Date,
-    validate: {
-      validator: function (date) {
-        date < Date.now();
-      },
-      message: "Birthdate cannot be today or in future!",
-    },
-    required: true,
-  },
-  address: { type: String, minlength: 5, maxlength: 255, required: true },
-  contact: contactSchema,
-  guardianInfo: {
-    name: { type: String, minlength: 3, maxlength: 75, required: true },
-    contact: contactSchema,
-  },
-  isActive: { type: Boolean, default: true },
-  semFeesPaid: { type: Boolean, default: false },
-  hasLibraryAccess: { type: Boolean, default: false },
-  doingSports: { type: Boolean, default: false },
-  inClubs: { type: Boolean, default: false },
-});
-
-const Student = mongoose.model("Student", studentSchema);
 
 //Get
 router.get("/", (req, res) => {
@@ -65,35 +23,6 @@ router.get("/students/:studentId", async (req, res) => {
 
 //POST
 router.post("/students", async (req, res) => {
-  const contactSchema = Joi.object({
-    phone: Joi.string().min(10).max(15).required(),
-    email: Joi.string().email().min(5).max(255).required(),
-  });
-
-  const schema = Joi.object({
-    studentId: Joi.number().integer().positive().required(),
-    firstName: Joi.string().min(1).max(50).required(),
-    lastName: Joi.string().min(1).max(50).required(),
-    gender: Joi.string()
-      .valid("male", "female")
-      .message("Gender must be either male or female")
-      .required(),
-    dateOfBirth: Joi.date()
-      .max("now")
-      .message("Date of birth cannot be tody or in the future")
-      .required(),
-    contact: contactSchema,
-    guardianInfo: Joi.object({
-      name: Joi.string().min(3).max(75).required(),
-      contact: contactSchema,
-    }),
-    isActive: Joi.boolean(),
-    semFeesPaid: Joi.boolean(),
-    hasLibraryAccess: Joi.boolean(),
-    doingSports: Joi.boolean(),
-    inSports: Joi.boolean(),
-  });
-
   const student = req.body;
   const { error } = schema.validate(student);
 
@@ -102,7 +31,7 @@ router.post("/students", async (req, res) => {
     const newStudent = await Student.create(student);
     res.status(201).send(newStudent);
   } catch (error) {
-    console.error(error);
+    console.error(error); //
     res.status(500).send("Internal server error");
   }
 });
